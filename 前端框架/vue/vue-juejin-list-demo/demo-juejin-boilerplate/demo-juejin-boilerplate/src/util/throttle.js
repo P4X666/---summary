52cfa09b00e932830413a91707e57aa055d0dbc3
P4X666 <2644378911@qq.com>
2020-11-16 19:50:08 +0800

    elementui alert 组件源码分析

--- 前端框架/vue/vue-juejin-list-demo/demo-juejin-boilerplate/demo-juejin-boilerplate/src/util/throttle.js
+++
@@ -1,9 +0,0 @@
-export function throttle(fn, timeout = 10) {
-  let timer;
-  return function(...args) {
-    if (timer) {
-      timer = clearTimeout(timer);
-    }
-    timer = setTimeout(() => fn.apply(this, [...args]), timeout);
-  };
-}
--- 前端框架/vue/vue-juejin-list-demo/demo-juejin-boilerplate/demo-juejin-boilerplate/test.js
+++
@@ -1,6 +0,0 @@
-function test_push(a,b,c) {
-    console.log(arguments)
-    Array.prototype.push.call(arguments,4)
-    console.log(arguments,1)
-}
-test_push(1,2,3)
